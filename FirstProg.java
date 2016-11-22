085a4bdfda09e2caa6ed1c22e840e22e361fb403
Nik M <nmarchese@gmail.com>
2016-11-22 15:54:36 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "I, Nik Marchese, am determined to learn how to code." );
+		System.out.println( "Today's date is Tuesday November 22, 2016." );
+	}
+}
