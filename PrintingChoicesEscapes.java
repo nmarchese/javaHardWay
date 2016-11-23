7c8f4766bc5c0e366275023868910438c5c969d9
Nik M <nmarchese@gmail.com>
2016-11-22 17:39:37 -0700

    Adding exercise 4 and its Study Drill

---
+++ PrintingChoicesEscapes.java
@@ -0,0 +1,19 @@
+public class PrintingChoicesEscapes {
+	public static void main( String[] args ) {
+		System.out.print( "Alpha\n" );
+		System.out.print( "Brovo\n" );
+		System.out.print( "Charlie\n" );
+		System.out.print( "Delta\n\n" );
+		System.out.print( "Echo" );
+		System.out.print( "Foxtrot" );
+		System.out.print( "Golf\n" );
+		System.out.print( "Hotel\n" );
+		System.out.print( "India\n\n" );
+		System.out.print( "This" + " " + "is" + " " + "a" + " test.\n" );
+		System.out.print("I" + " " + "am learning " + "Java" + " the Hard" + " " + "Way\n" );
+	// I removed useless "System.out.print" statements and empty lines.
+	// Also, I didn't use any "println" statements even though the study drill afforded me one
+	// And I could have used a block comment here instead of two seperate line comments and a third explaining
+	}
+
+}
