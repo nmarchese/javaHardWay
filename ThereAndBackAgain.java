5bc33b6f40e157dbc5d2ae3b33918741dad4de55
Nik M <nmarchese@gmail.com>
2016-11-26 13:32:38 -0700

    Exercise 33 revised

--- ThereAndBackAgain.java
+++ ThereAndBackAgain.java
@@ -1,13 +1,16 @@
 public class ThereAndBackAgain {
 	public static void main( String[] args ) {
 		System.out.println("Here.");
-		ThereAndBackAgain.erbor();  // Added class "ThereAndBackAgain". Would not comple as written by book.
+		erebor();  
 		System.out.println("Back first time.");
-		ThereAndBackAgain.erbor();  // Went to my roommate for help and this is how we got around the compiler error.
+		erebor();
 		System.out.println("Back second time.");
 	}
 
-	public static void erbor() {
+	public static void erebor() {
 		System.out.println("There.");
+
+	/*There had been a typo causing compiler error in previous code.
+	Found the error, no need to define class before calling erebor function */
 	}
 }
